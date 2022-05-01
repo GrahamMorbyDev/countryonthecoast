d690ca35eec3e6f91cbedacc6106fe24d7493b29
Graham Morby <grahammorbydev@gmail.com>
2022-05-01 18:02:47 +0100

    added artists to db

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Artist
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status','created_on')
@@ -7,4 +7,11 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'link', 'created_on')
+    list_filter = ("name",)
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Artist, ArtistAdmin)
