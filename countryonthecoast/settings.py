4bec71fefe827ce1566da231b343291a0277ae1c
Graham Morby <grahammorbydev@gmail.com>
2022-05-03 18:43:05 +0100

    added tailwind and compressor

--- countryonthecoast/settings.py
+++ countryonthecoast/settings.py
@@ -18,6 +18,13 @@ TEMPLATE_DIRS = [
     os.path.join(BASE_DIR, 'templates'),
 ]
 
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -35,12 +42,6 @@ MEDIA_URL = '/media/'
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-# Static file route
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,7 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'compressor'
 ]
 
 MIDDLEWARE = [
